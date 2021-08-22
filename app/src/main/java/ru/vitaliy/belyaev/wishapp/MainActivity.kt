98952b2809c55ce22547d0f576c96114969c2bba
Vitaliy Belyaev <belyaev1392@gmail.com>
2021-08-22 19:43:51 +0300

    Initial commit

---
+++ app/src/main/java/ru/vitaliy/belyaev/wishapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package ru.vitaliy.belyaev.wishapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.vitaliy.belyaev.wishapp.ui.theme.WishAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WishAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WishAppTheme {
+        Greeting("Android")
+    }
+}
