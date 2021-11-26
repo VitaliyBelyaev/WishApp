6ec7953512e473b090abe9e499702c5ee767eb5c
Vitaliy Belyaev <belyaev1392@gmail.com>
2021-11-26 22:56:00 +0300

    Add backup description

--- app/src/main/java/ru/vitaliy/belyaev/wishapp/ui/screens/settings/components/BackupSheetContent.kt
+++ app/src/main/java/ru/vitaliy/belyaev/wishapp/ui/screens/settings/components/BackupSheetContent.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import ru.vitaliy.belyaev.wishapp.R
 
 @Composable
 fun BackupSheetContent() {
 
     Text(
-        text = "backup",
-        modifier = Modifier.padding(16.dp)
+        text = stringResource(R.string.backup_description),
+        modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 24.dp)
     )
 }
