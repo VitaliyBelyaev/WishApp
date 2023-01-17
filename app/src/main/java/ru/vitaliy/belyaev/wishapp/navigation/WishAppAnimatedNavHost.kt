133a82f696ba2c9b53500a5c4dbfdc3b23379cc4
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-01-17 21:35:35 +0600

    Fix blinking in navigation

--- app/src/main/java/ru/vitaliy/belyaev/wishapp/navigation/WishAppAnimatedNavHost.kt
+++ app/src/main/java/ru/vitaliy/belyaev/wishapp/navigation/WishAppAnimatedNavHost.kt
@@ -1,12 +1,15 @@
 package ru.vitaliy.belyaev.wishapp.navigation
 
 import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.VisibilityThreshold
 import androidx.compose.animation.core.spring
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.IntOffset
+import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.AnimatedNavHost
@@ -20,55 +23,72 @@ fun WishAppAnimatedNavHost(
     builder: NavGraphBuilder.() -> Unit
 ) {
     val dampingRatio: Float = Spring.DampingRatioNoBouncy
-    val stiffness: Float = 400f
+    val stiffness = 1750f
     val visibilityThreshold: IntOffset = IntOffset.VisibilityThreshold
 
+    // Оставил красивые анимации похожие на те что в телеге, не стал юзать пока, так как
+    // на девайсе жутко тормозило при возвращении на "тяжелые экраны"
+    val enter: (AnimatedContentScope<NavBackStackEntry>.() -> EnterTransition) = {
+        slideIntoContainer(
+            towards = AnimatedContentScope.SlideDirection.Left,
+            animationSpec = spring(
+                dampingRatio = dampingRatio,
+                stiffness = stiffness,
+                visibilityThreshold = visibilityThreshold
+            )
+        )
+    }
+
+    val exit: (AnimatedContentScope<NavBackStackEntry>.() -> ExitTransition) = {
+        slideOutOfContainer(
+            towards = AnimatedContentScope.SlideDirection.Left,
+            animationSpec = spring(
+                dampingRatio = dampingRatio,
+                stiffness = stiffness,
+                visibilityThreshold = visibilityThreshold
+            ),
+            targetOffset = {
+                (it * 0.1f).roundToInt()
+            }
+        )
+    }
+
+    val popEnter: (AnimatedContentScope<NavBackStackEntry>.() -> EnterTransition) = {
+        slideIntoContainer(
+            towards = AnimatedContentScope.SlideDirection.Right,
+            animationSpec = spring(
+                dampingRatio = dampingRatio,
+                stiffness = stiffness,
+                visibilityThreshold = visibilityThreshold
+            ),
+            initialOffset = {
+                (it * 0.1f).roundToInt()
+            }
+        )
+    }
+
+    val popExit: (AnimatedContentScope<NavBackStackEntry>.() -> ExitTransition) = {
+        slideOutOfContainer(
+            towards = AnimatedContentScope.SlideDirection.Right,
+            animationSpec = spring(
+                dampingRatio = dampingRatio,
+                stiffness = stiffness,
+                visibilityThreshold = visibilityThreshold
+            )
+        )
+    }
+
+    // Вырубил анимации просто, так как обычный NavHost тупо юзает
+    // Crossfade(androidx.navigation:navigation-compose:2.5.3), которым вообще никак
+    // нельзя управлять и если у приложения тема отличная от темы девайса или это девайс на
+    // пре 10 андроиде, то этот кроссфейд видно как мигание.
     AnimatedNavHost(
         navController = navController,
         startDestination = startDestination,
-        enterTransition = {
-            slideIntoContainer(
-                towards = AnimatedContentScope.SlideDirection.Left,
-                animationSpec = spring(
-                    dampingRatio = dampingRatio,
-                    stiffness = stiffness,
-                    visibilityThreshold = visibilityThreshold
-                )
-            )
-        },
-        exitTransition = {
-            slideOutOfContainer(
-                towards = AnimatedContentScope.SlideDirection.Left,
-                animationSpec = spring(
-                    dampingRatio = dampingRatio,
-                    stiffness = stiffness,
-                    visibilityThreshold = visibilityThreshold
-                ),
-                targetOffset = {
-                    (it * 0.65).roundToInt()
-                }
-            )
-        },
-        popEnterTransition = {
-            slideIntoContainer(
-                towards = AnimatedContentScope.SlideDirection.Right,
-                animationSpec = spring(
-                    dampingRatio = dampingRatio,
-                    stiffness = stiffness,
-                    visibilityThreshold = visibilityThreshold
-                )
-            )
-        },
-        popExitTransition = {
-            slideOutOfContainer(
-                towards = AnimatedContentScope.SlideDirection.Right,
-                animationSpec = spring(
-                    dampingRatio = dampingRatio,
-                    stiffness = stiffness,
-                    visibilityThreshold = visibilityThreshold
-                )
-            )
-        },
+        enterTransition = { EnterTransition.None },
+        exitTransition = { ExitTransition.None },
+        popEnterTransition = { EnterTransition.None },
+        popExitTransition = { ExitTransition.None },
         builder = builder
     )
 }
