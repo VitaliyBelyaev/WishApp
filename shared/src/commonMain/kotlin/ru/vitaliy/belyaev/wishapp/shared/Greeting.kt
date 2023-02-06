08222d8a7412fe3c88e55ba298746616129bbd19
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-02-06 18:18:45 +0600

    Refactoring

--- shared/src/commonMain/kotlin/ru/vitaliy/belyaev/wishapp/shared/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package ru.vitaliy.belyaev.wishapp.shared
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
---
+++ shared/src/commonMain/kotlin/ru/vitaliy/belyaev/wishapp/shared/WishAppSdk.kt
@@ -0,0 +1,5 @@
+package ru.vitaliy.belyaev.wishapp.shared
+
+class WishAppSdk {
+
+}
