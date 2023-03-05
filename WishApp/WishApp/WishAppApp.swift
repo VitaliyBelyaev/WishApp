1af18a0729bb1fdaa3e47106089699e73a241885
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-03-05 22:09:16 +0600

    Add ios app

---
+++ WishApp/WishApp/WishAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishAppApp.swift
+//  WishApp
+//
+//  Created by Vitaliy on 05.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WishAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
