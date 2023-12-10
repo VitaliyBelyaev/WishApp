7a4e7b7e773559d44731a60d9f78bc63e6a36133
Vitaliy Belyaev <belyaev1392@gmail.com>
2023-12-10 22:13:04 +0600

    Add localizations

--- app/src/main/java/ru/vitaliy/belyaev/wishapp/ui/screens/wish_list/components/EmptyWishesPlaceholer.kt
+++ app/src/main/java/ru/vitaliy/belyaev/wishapp/ui/screens/wish_list/components/EmptyWishesPlaceholer.kt
@@ -6,19 +6,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import ru.vitaliy.belyaev.wishapp.R
 
 @Composable
 fun EmptyWishesPlaceholder(
@@ -47,7 +45,7 @@ fun EmptyWishesPlaceholder(
             Divider()
             Spacer(modifier = Modifier.height(16.dp))
             Text(
-                text = "Или восстановите свои данные из резервной копии",
+                text = stringResource(R.string.backup_wishlist_empty_text),
                 color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f),
                 style = MaterialTheme.typography.bodyLarge,
                 textAlign = TextAlign.Center
@@ -57,7 +55,7 @@ fun EmptyWishesPlaceholder(
                 onClick = onGoToBackupScreenClicked,
                 modifier = Modifier.padding(top = 8.dp)
             ) {
-                Text(text = "Перейти в резервное копирование")
+                Text(text = stringResource(R.string.backup_wishlist_go_to_backup_button_text))
             }
         }
     }
