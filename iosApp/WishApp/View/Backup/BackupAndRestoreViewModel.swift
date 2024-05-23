7618fd0f70b14eb19edb16949407bd9c6006db03
Vitaliy Belyaev <v.belyaev@prequelapp.com>
2024-05-23 22:47:22 +0600

    Add logic to copy db contents

--- iosApp/WishApp/View/Backup/BackupAndRestoreViewModel.swift
+++ iosApp/WishApp/View/Backup/BackupAndRestoreViewModel.swift
@@ -13,11 +13,168 @@ final class BackupAndRestoreViewModel: ObservableObject {
     
     private let sdk: WishAppSdk = WishAppSdkDiHelper().wishAppSdk
     
+    private let localBackupsDirName = "local_backups"
+    private let localBackupDbName = "backup.db"
+    
+    
     init() {
        logDirContents()
     }
     
     func onRestoreClicked() {
+        restoreBackupFromLocal()
+    }
+    
+    func onCreateBackupClicked() {
+        createBackupToLocalFileSystem()
+    }
+    
+    private func createBackupToLocalFileSystem() {
+        print("createBackupToLocalFileSystem START")
+        logDirContents()
+        
+        let localBackupDirUrl = URL.applicationSupportDirectory.appendingPathComponent(localBackupsDirName)
+        FileManager.default.createDirIfNotExists(dirUrl: localBackupDirUrl)
+        
+        print("localBackupDirUrl: \(localBackupDirUrl)")
+        
+        let timestamp = NSDate().timeIntervalSince1970
+        let backupFileUrl = localBackupDirUrl.appendingPathComponent(localBackupDbName)
+        print("backupFileUrl: \(backupFileUrl)")
+        
+        let originFileUrl = URL.applicationSupportDirectory.appendingPathComponent("databases/ru_vitaliy_belyaev_wishapp.db")
+        print("originFileUrl: \(originFileUrl)")
+        
+        var isDir: ObjCBool = false
+        
+        if FileManager.default.fileExists(atPath: backupFileUrl.path, isDirectory: &isDir) {
+            print("Backup file exists in local, backupFileUrl: \(backupFileUrl)")
+            
+            do {
+                print("Try to remove backup from local backupFileUrl: \(backupFileUrl)")
+                try FileManager.default.removeItem(at: backupFileUrl)
+            }
+            catch {
+                //Error handling
+                print("Error remove backup from local backupFileUrl: \(backupFileUrl)")
+            }
+        }
+        
+        
+        do {
+            print("Try to copy backup from app to local storage")
+            try FileManager.default.copyItem(at: originFileUrl, to: backupFileUrl)
+            print("copy backup from app to local storage done")
+        }
+        catch {
+            //Error handling
+            print("Error in copy item:\(error.localizedDescription)")
+        }
+        
+        logDirContents()
+        
+        print("createBackupToLocalFileSystem END")
+    }
+    
+    private func createBackupToICloud() {
+        if let containerUrl = FileManager.default.getAppContainerUrlInICloud() {
+            print("containerUrl: \(containerUrl)")
+            
+            FileManager.default.createDirIfNotExists(dirUrl: containerUrl)
+            
+            let cloudFileUrl = containerUrl.appendingPathComponent("wishapp.backup")
+            print("cloudFileUrl: \(cloudFileUrl)")
+            
+            let originFileUrl = URL.applicationSupportDirectory.appendingPathComponent("databases/ru_vitaliy_belyaev_wishapp.db")
+            
+            print("originFileUrl: \(originFileUrl)")
+            
+            var isDir: ObjCBool = false
+            
+            if FileManager.default.fileExists(atPath: cloudFileUrl.path, isDirectory: &isDir) {
+                print("Backup file exists in cloud, cloudFileUrl: \(cloudFileUrl)")
+                
+                do {
+                    print("Try to remove backup from cloud cloudFileUrl: \(cloudFileUrl)")
+                    try FileManager.default.removeItem(at: cloudFileUrl)
+                }
+                catch {
+                    //Error handling
+                    print("Error remove backup from cloud cloudFileUrl: \(cloudFileUrl)")
+                }
+            }
+            
+            do {
+                print("Try to copy backup from local to cloud")
+                try FileManager.default.copyItem(at: originFileUrl, to: cloudFileUrl)
+                print("copy backup from local to cloud done")
+            }
+            catch {
+                //Error handling
+                print("Error in copy item:\(error.localizedDescription)")
+            }
+        } else {
+            print("onCreateBackupClicked, no app container in iCloud")
+        }
+    }
+    
+    private func restoreBackupFromLocal() {
+        print("restoreBackupFromLocal START")
+        
+        let localBackupDirUrl = URL.applicationSupportDirectory.appendingPathComponent(localBackupsDirName)
+        FileManager.default.createDirIfNotExists(dirUrl: localBackupDirUrl)
+        
+        print("localBackupDirUrl: \(localBackupDirUrl)")
+        
+        let backupFileUrl = localBackupDirUrl.appendingPathComponent(localBackupDbName)
+        print("backupFileUrl: \(backupFileUrl)")
+        
+        let originFileUrl = URL.applicationSupportDirectory.appendingPathComponent("databases/ru_vitaliy_belyaev_wishapp.db")
+        print("originFileUrl: \(originFileUrl)")
+        
+        let backupFileUrlInDbDir = URL.applicationSupportDirectory.appendingPathComponent("databases/\(localBackupDbName)")
+        print("backupFileUrlInDbDir: \(backupFileUrlInDbDir)")
+        
+            do {
+                // copy backup db to database dir
+                try FileManager.default.copyItem(at: backupFileUrl, to: backupFileUrlInDbDir)
+                
+                print("!!!!Copy local db to current databases dir done")
+                logDirContents()
+    
+                // copy contents of backup db to current db
+                let isSuccess = sdk.doCopyContentFromBackupDatabase(backupDbName: localBackupDbName)
+                print("!!!!Copy contents of backup db to current db done, isSuccess:\(isSuccess)")
+                
+                
+                print("Try to remove localDbFile in databases dir")
+                try FileManager.default.removeItem(at: backupFileUrlInDbDir)
+                
+                logDirContents()
+            } catch let error{
+                //Error handling
+                print("Error in resotre backup:\(error)")
+            }
+        
+        
+//        do {
+//            print("Try to copy backup from cloud to local")
+//            try FileManager.default.copyItem(at: localBackupDirUrl, to: originFileUrl)
+//            print("Copy done")
+//            logDirContents()
+//            
+//            sdk.reopenDatabase()
+//        }
+//        catch {
+//            //Error handling
+//            print("Error in copy item:\(error.localizedDescription)")
+//        }
+        
+        print("restoreBackupFromLocal END")
+    
+    }
+    
+    private func restoreBackupFromICloud() {
         if let containerUrl = FileManager.default.getAppContainerUrlInICloud() {
             
             
@@ -77,48 +234,6 @@ final class BackupAndRestoreViewModel: ObservableObject {
         }
     }
     
-    func onCreateBackupClicked() {
-        if let containerUrl = FileManager.default.getAppContainerUrlInICloud() {
-            print("containerUrl: \(containerUrl)")
-            
-            FileManager.default.createDirIfNotExists(dirUrl: containerUrl)
-            
-            let cloudFileUrl = containerUrl.appendingPathComponent("wishapp.backup")
-            print("cloudFileUrl: \(cloudFileUrl)")
-            
-            let originFileUrl = URL.applicationSupportDirectory.appendingPathComponent("databases/ru_vitaliy_belyaev_wishapp.db")
-            
-            print("originFileUrl: \(originFileUrl)")
-            
-            var isDir: ObjCBool = false
-            
-            if FileManager.default.fileExists(atPath: cloudFileUrl.path, isDirectory: &isDir) {
-                print("Backup file exists in cloud, cloudFileUrl: \(cloudFileUrl)")
-                
-                do {
-                    print("Try to remove backup from cloud cloudFileUrl: \(cloudFileUrl)")
-                    try FileManager.default.removeItem(at: cloudFileUrl)
-                }
-                catch {
-                    //Error handling
-                    print("Error remove backup from cloud cloudFileUrl: \(cloudFileUrl)")
-                }
-            }
-            
-            do {
-                print("Try to copy backup from local to cloud")
-                try FileManager.default.copyItem(at: originFileUrl, to: cloudFileUrl)
-                print("copy backup from local to cloud done")
-            }
-            catch {
-                //Error handling
-                print("Error in copy item:\(error.localizedDescription)")
-            }
-        } else {
-            print("onCreateBackupClicked, no app container in iCloud")
-        }
-    }
-    
     private func logDirContents() {
         do {
             
@@ -130,26 +245,53 @@ final class BackupAndRestoreViewModel: ObservableObject {
                 print("Found support item: \(supItem)")
             }
             
-            
-            let dbDirUrl = URL.applicationSupportDirectory.appendingPathComponent("databases")
-            
-            let dbItems = try FileManager.default.contentsOfDirectory(atPath: dbDirUrl.path)
-            
-            for dbItem in dbItems {
+            let localBackupsDirUrl = URL.applicationSupportDirectory.appendingPathComponent(localBackupsDirName)
+            let localBackupsItems = try FileManager.default.contentsOfDirectory(atPath: localBackupsDirUrl.path)
+            for backupItem in localBackupsItems {
+                print("Found local backup item: \(backupItem)")
                 
-                if dbItem == "ru_vitaliy_belyaev_wishapp.db" {
+                if backupItem == "backup.db" {
                     do {
-                        let dbFileUrl = dbDirUrl.appendingPathComponent(dbItem)
+                        let fileUrl = localBackupsDirUrl.appendingPathComponent(backupItem)
                         
-                        let map = try FileManager.default.attributesOfItem(atPath: dbFileUrl.path)
+                        let map = try FileManager.default
+                            .attributesOfItem(atPath: fileUrl.path)
+                            .sorted() { $0.key.rawValue < $1.key.rawValue }
                         
-                        print("dbFileUrl attrs: \(map)")
+                        map.forEach { (key: FileAttributeKey, value: Any) in
+                            print("local backup file attr, key: \(key), value: \(value)")
+                        }
                     } catch {
                         
                     }
                 }
                 
+                
+            }
+            
+            let dbDirUrl = URL.applicationSupportDirectory.appendingPathComponent("databases")
+            let dbItems = try FileManager.default.contentsOfDirectory(atPath: dbDirUrl.path)
+            for dbItem in dbItems {
+                
                 print("Found db item: \(dbItem)")
+                
+                if dbItem == "ru_vitaliy_belyaev_wishapp.db" {
+                    do {
+                        let dbFileUrl = dbDirUrl.appendingPathComponent(dbItem)
+                        
+                        let map = try FileManager.default
+                            .attributesOfItem(atPath: dbFileUrl.path)
+                            .sorted() { $0.key.rawValue < $1.key.rawValue }
+                    
+                    
+                        map.forEach { (key: FileAttributeKey, value: Any) in
+                            print("dbFileUrl attr, key: \(key), value: \(value)")
+                        }
+                        
+                    } catch {
+                        
+                    }
+                }
             }
         } catch {
             // failed to read directory – bad permissions, perhaps?
--- shared/src/commonMain/kotlin/ru/vitaliy/belyaev/wishapp/shared/data/WishAppSdk.kt
+++ shared/src/commonMain/kotlin/ru/vitaliy/belyaev/wishapp/shared/data/WishAppSdk.kt
@@ -1,11 +1,14 @@
 package ru.vitaliy.belyaev.wishapp.shared.data
 
 import app.cash.sqldelight.db.SqlDriver
-import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import ru.vitaliy.belyaev.wishapp.shared.data.coroutines.getDispatcherProvider
 import ru.vitaliy.belyaev.wishapp.shared.data.database.DatabaseDriverFactory
+import ru.vitaliy.belyaev.wishapp.shared.data.database.ImageQueries
+import ru.vitaliy.belyaev.wishapp.shared.data.database.TagQueries
 import ru.vitaliy.belyaev.wishapp.shared.data.database.WishAppDb
+import ru.vitaliy.belyaev.wishapp.shared.data.database.WishQueries
+import ru.vitaliy.belyaev.wishapp.shared.data.database.WishTagRelationQueries
 import ru.vitaliy.belyaev.wishapp.shared.data.repository.DatabaseRepository
 
 class WishAppSdk(private val databaseDriveFactory: DatabaseDriverFactory) {
@@ -15,6 +18,20 @@ class WishAppSdk(private val databaseDriveFactory: DatabaseDriverFactory) {
     private var _databaseRepository: DatabaseRepository? = null
 
     private var sqlDriver: SqlDriver? = null
+
+    private var database: WishAppDb? = null
+
+    private val wishQueries: WishQueries?
+        get() = database?.wishQueries
+
+    private val wishTagRelationQueries: WishTagRelationQueries?
+        get() = database?.wishTagRelationQueries
+
+    private val tagQueries: TagQueries?
+        get() = database?.tagQueries
+
+    private val imageQueries: ImageQueries?
+        get() = database?.imageQueries
 
     init {
 
@@ -40,7 +57,55 @@ class WishAppSdk(private val databaseDriveFactory: DatabaseDriverFactory) {
         Napier.d("Reopen database")
         closeDatabase()
         sqlDriver = databaseDriveFactory.createDatabaseDriver()
-        val database = WishAppDb(sqlDriver!!)
+        val database = WishAppDb(sqlDriver!!).also {
+            this.database = it
+        }
         _databaseRepository = DatabaseRepository(database, getDispatcherProvider())
+    }
+
+    fun copyContentFromBackupDatabase(backupDbName: String): Boolean {
+        Napier.d("copyContentFromBackupDatabase, backupDbName: $backupDbName")
+
+
+        return try {
+            attachBackupDatabase(backupDbName)
+
+            wishQueries?.transaction {
+                wishQueries?.insertFromBackupDb()
+            }
+
+            detachBackupDatabase()
+            true
+        } catch (error: Throwable) {
+            Napier.e(message = "Error while copying content from backup database", throwable = error)
+            false
+        }
+    }
+
+    fun attachBackupDatabase(backupDbName: String) {
+        Napier.d("attachBackupDatabase, backupDbName: $backupDbName")
+
+        sqlDriver!!.execute(
+            identifier = null,
+            sql = "ATTACH DATABASE ? AS $BACKUP_DATABASE_SCHEMA_NAME",
+            parameters = 1
+        ) {
+            bindString(0, backupDbName)
+        }
+    }
+
+    fun detachBackupDatabase() {
+        Napier.d("detachBackupDatabase")
+
+        sqlDriver!!.execute(
+            identifier = null,
+            sql = "DETACH DATABASE $BACKUP_DATABASE_SCHEMA_NAME",
+            parameters = 0
+        )
+    }
+
+    companion object {
+
+        private const val BACKUP_DATABASE_SCHEMA_NAME = "backupDb"
     }
 }
