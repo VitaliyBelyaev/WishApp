d89f80eb7ef69805046ae952380dfd8d3feebaeb
Vitaliy Belyaev <v.belyaev@prequelapp.com>
2024-07-13 18:47:01 +0600

    Update app version to 1.8.2(24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "ru.vitaliy.belyaev.wishapp"
         minSdk = 23
         targetSdk = 34
-        versionCode = 23
-        versionName = "1.8.1"
+        versionCode = 24
+        versionName = "1.8.2"
 
         stringConfigField(amplitudeApiKey, amplitudeApiKeyValue)
 
